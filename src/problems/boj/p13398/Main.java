a0dc69145c2ea1e7cba3a19e9e587b9531b6a9a0
k0081915 <k0081915@gmail.com>
2026-03-02 18:09:37 +0900

    init

---
+++ src/problems/boj/p13398/Main.java
@@ -0,0 +1,16 @@
+package problems.boj.p13398;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+        int n = Integer.parseInt(br.readLine());
+    }
+}
